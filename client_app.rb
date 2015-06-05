cbbc5820c1ca166a19b991c7a597839aebb43036
Endika Gutiérrez <me@endika.net>
2015-06-05 18:22:47 +0200

    Device motion events sent through WS

--- client_app.rb
+++ client_app.rb
@@ -5,4 +5,19 @@ class ClientApp < Sinatra::Base
   get '/' do
     erb :index
   end
+
+  get '/socket' do
+    not_found unless request.websocket?
+    request.websocket do |ws|
+      ws.onopen do
+
+      end
+      ws.onmessage do |msg|
+        puts msg
+      end
+      ws.onclose do
+
+      end
+    end
+  end
 end
--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,1 +1,24 @@
-alert("Hello world");
+var connection = new WebSocket('ws://' + location.host + '/socket');
+
+connection.onopen = function () {
+  connection.send('Hello server!');
+};
+
+// Log errors
+connection.onerror = function (error) {
+  console.log('WebSocket Error ' + error);
+};
+
+// Log messages from the server
+connection.onmessage = function (e) {
+  console.log('Server: ' + e.data);
+};
+
+window.ondevicemotion = function(event) {
+  connection.send(JSON.stringify(event));
+};
+
+window.ondeviceorientation = function(event) {
+  connection.send(JSON.stringify(event));
+};
+
