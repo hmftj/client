da567ef6bc53d3bfb9104aa2c8a3237bb0f9278a
Endika Gutiérrez <me@endika.net>
2015-06-20 19:34:11 +0200

    Added eventmachine for connection with backend

--- client_app.rb
+++ client_app.rb
@@ -2,8 +2,14 @@ require 'sinatra/base'
 require 'sinatra-websocket'
 require 'json'
 require 'matrix'
+require 'eventmachine'
+
+EventMachine.next_tick do
+  $conn = EventMachine.connect_unix_domain("/tmp/drone-ctrl.sock")
+end
 
 class ClientApp < Sinatra::Base
+
   get '/' do
     erb :index
   end
@@ -12,41 +18,12 @@ class ClientApp < Sinatra::Base
     not_found unless request.websocket?
     request.websocket do |ws|
       ws.onopen do
-
       end
       ws.onmessage do |msg|
-        event = JSON.parse(msg, symbolize_names: true)
-        #puts event
-        case event[:event]
-        when 'devicemotion'
-          handle_devicemotion(event[:data])
-        when 'deviceorientation'
-          handle_deviceorientation(event[:data])
-        when 'joystickmove'
-          p event[:data]
-        end
+        $conn.send_data(msg)
       end
       ws.onclose do
-
       end
     end
   end
-end
-
-def handle_devicemotion(event)
-  x, y, z = event[:accelerationIncludingGravity].values_at(:x, :y, :z)
-
-  alpha, beta, gamma = event[:rotationRate]
-    .values_at(:alpha, :beta, :gamma)
-
-
-  acceleration = Vector[x, y, z]
-  rotation = Vector[alpha, beta, gamma]
-  puts acceleration
-  puts rotation
-end
-
-def handle_deviceorientation(event)
-  heading = event[:webkitCompassHeading]
-  puts heading
 end
